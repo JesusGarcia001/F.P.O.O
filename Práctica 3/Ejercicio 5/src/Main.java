c6613f8d0e06b0d74bc0f2a343305db335e89327
JesusGarcia001 <123047376@upq.edu.mx>
2025-01-28 15:20:38 -0600

    Ejercicio 5 Completado

---
+++ Práctica 3/Ejercicio 5/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class pesopaquete{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.print("Introduce el número de payasos vendidos: ");
+
+        int payasos = scanner.nextInt();
+        System.out.print("Introduce el número de muñecas vendidas: ");
+        int munecas = scanner.nextInt();
+
+        int pesopayasos = payasos * 112;
+        int pesomunecas = munecas * 75;
+
+        int pesototal = pesopayasos + pesomunecas;
+
+        System.out.println("El peso total del paquete es: "+ pesototal + " gramos");
+    }
+}
