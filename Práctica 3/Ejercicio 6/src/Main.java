859f200978aff7a240241e6c018ba1a39f4b554c
JesusGarcia001 <123047376@upq.edu.mx>
2025-01-28 15:24:56 -0600

    Ejercicio 6 completado

---
+++ Práctica 3/Ejercicio 6/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class InvertirFrase{
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce una frase: ");
+        String frase = scanner.nextLine();
+
+        String fraseinvertida = new StringBuilder(frase).reverse().toString();
+
+        System.out.println("La frase invertida es: "+ fraseinvertida);
+
+    }
+}
